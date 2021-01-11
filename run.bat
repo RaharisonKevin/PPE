5883bf0028715114df90e34dd5f21de6c0242d4a
RaharisonKevin <rkevinandy@yahoo.fr>
2021-01-11 08:15:49 +0100

    first commit

---
+++ run.bat
@@ -0,0 +1,8 @@
+cd C:\Users\Kevin\Desktop\PPE
+git init
+git add *
+git commit -m "first commit"
+git remote add origin https://github.com/RaharisonKevin/PPE.git
+git branch -M main
+git push -u origin main
+pause
