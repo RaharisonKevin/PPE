6fefdaa3ca0677db3e863211fde7244686fb241f
RaharisonKevin <rkevinandy@yahoo.fr>
2021-01-25 09:04:51 +0100

    first commit

--- src/header.inc.php
+++ src/header.inc.php
@@ -1,9 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>France Montagnes</title>
-    <link rel="stylesheet" href="./css/style.css">
-</head>
+<header>
+        <div role="navigation">
+        <img class="logo" src="./asset/logo.png" alt="logo">
+        <nav>  
+        <ul>
+            <li><a href="#guide">GUIDE DES STATIONS</a></li>
+            <li><a href="#" >MAGAZINE</a></li>
+            <li><a href="#">INFO LIVE</a></li>
+            <li><a href="#reservation">RÉSERVATION</a></li>
+            <li><a href="#" id="loop"><img src="./asset/loop.png" alt="recherche"></a></li>
+        </ul>
+        </nav>
+        </div>
+    </header>
